d2686043c9dd4ca46ece872f4c14806a9f5ed181
Shayan Salehi <79784127+eyepatchss@users.noreply.github.com>
2021-04-26 23:05:04 +0430

    Update SpellTrap.java

--- Project/src/Model 1/Model/SpellTrap.java
+++ Project/src/Model 1/Model/SpellTrap.java
@@ -1,19 +1,20 @@
 ﻿package Model 1;
 
- class SpellTrap extends Card
-{
-    private String type;		
-    private String icon;		
+public class SpellTrap extends Card {
+    private String spellType;		
+    private String icon;	
 
     
-    public <<construcrot>> SpellTrap(String name, String description, String type, String cardNumber, String type, String icon) 		
+    public SpellTrap(String name, String description, String type, String cardNumber, String spellType, String icon) 		
     {
-        
+        super(name, description, type, cardNumber);
+        this.spellType = spellType;
+        this.icon = icon;
     }		
     
-    public void setType(String type) 		
+    public void setSpellType(String spellType) 		
     {
-        
+        this.spellType = spellType;
     }		
     
     public void setIcon(String icon) 		
