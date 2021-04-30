365520ad63233085addacccee4728b8ef05be0db
mahdi-shafee <82397556+mahdi-shafee@users.noreply.github.com>
2021-05-01 03:41:58 +0430

    Update Scoreboard.java

--- Project/src/main/java/Menus/Scoreboard.java
+++ Project/src/main/java/Menus/Scoreboard.java
@@ -19,16 +19,16 @@ public class Scoreboard {
     }
 
     public void run() {
-        String command = null;
         while (true) {
+            String command = CommonTools.scan.nextLine();
             // TODO: 2021-04-25
             if (command.equals("scoreboard show"))
                 showBoard();
-             else if (command.equals("menu exit"))
+            else if (command.equals("menu exit"))
                 return;
             else if (command.matches("^menu enter (profile|duel|shop|scoreboard|deckmenu)$"))
                 System.out.println("menu navigation is not possible");
-             else
+            else
                 System.out.println("invalid command");
 
         }
@@ -38,8 +38,15 @@ public class Scoreboard {
     public void showBoard() {
         ArrayList<Player> players = Player.getPlayers();
         Collections.sort(players);
+        int lastScore = -1;
+        int position = 0;
         for (int i = 0; i < players.size(); i++) {
-            System.out.println(i + 1 + "- " + players.get(i).toString());
+            int score = players.get(i).getScore();
+            if (score != lastScore) {
+                position = position + 1;
+            }
+            System.out.println(position + "- " + players.get(i).toString());
+            lastScore = score;
         }
     }
 }
