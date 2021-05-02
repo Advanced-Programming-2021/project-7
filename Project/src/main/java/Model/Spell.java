ed0dffcc9a01b08234e8525eb435908c54377d8d
Shayan Salehi <79784127+eyepatchss@users.noreply.github.com>
2021-05-02 23:11:04 +0430

    Create Spell.java

---
+++ Project/src/main/java/Model/Spell.java
@@ -0,0 +1,41 @@
+package Model;
+
+public class Spell extends Card {
+    private String spellType;		
+    private String icon;	
+
+    
+    public Spell(String name, String description, String type, String spellType, String icon, int price) {
+        super(name, description, type, price);
+        this.spellType = spellType;
+        this.icon = icon;
+    }
+    
+    public void setSpellType(String spellType) {
+        this.spellType = spellType;
+    }
+    
+    public void setSpellIcon(String icon) {
+        this.icon = icon;
+    }
+    
+    public String getSpellType() {
+        return spellType;
+    }
+    
+    public String getSpellIcon() {
+        return icon;
+    }
+    
+    public String getSpellName() {
+        return name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getCardNumber() {
+        return cardNumber;
+    }	
+}
