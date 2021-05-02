de6a8bd234ad54fdba9982687d8e6c24e3ad2fe2
Shayan Salehi <79784127+eyepatchss@users.noreply.github.com>
2021-05-02 23:11:39 +0430

    Create Trap.java

---
+++ Project/src/main/java/Model/Trap.java
@@ -0,0 +1,41 @@
+package Model;
+
+public class Trap extends Card {
+    private String trapType;		
+    private String icon;	
+
+    
+    public Trap(String name, String description, String type, String trapType, String icon, int price) {
+        super(name, description, type, price);
+        this.trapType = trapType;
+        this.icon = icon;
+    }
+    
+    public void setTrapType(String trapType) {
+        this.trapType = trapType;
+    }
+    
+    public void setTrapIcon(String icon) {
+        this.icon = icon;
+    }
+    
+    public String getTrapType() {
+        return trapType;
+    }
+    
+    public String getTrapIcon() {
+        return icon;
+    }
+    
+    public String getTrapName() {
+        return name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getCardNumber() {
+        return cardNumber;
+    }	
+}
