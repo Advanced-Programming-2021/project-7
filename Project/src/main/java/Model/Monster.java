0361e08672268144d3bf5c78b1824398d6279f1b
Shayan <s.salehi1381@gmail.com>
2021-05-03 01:20:15 +0430

    card updated

--- Project/src/main/java/Model/Monster.java
+++ Project/src/main/java/Model/Monster.java
@@ -1,86 +1,71 @@
 package Model;
 
- public class Monster extends Card {
-    private int attackPoint;		
-    private int defensePoint;		
-    private int level;		
-    private String attribue;		
-    private String monsterType;		
+public class Monster extends Card {
+    private int attackPoint;
+    private int defensePoint;
+    private int level;
+    private String attribue;
+    private String monsterType;
 
-    
-    public Monster(String name, String description, String type, String cardNumber, int attackPoint, 
-    int defensePoint, int level, String attribute, String monsterType, int price) 		
-    {
-        super(name, description, type, cardNumber, price);
+    public Monster(String name, String description, String type, int attackPoint,
+                   int defensePoint, int level, String attribute, String monsterType, int price) {
+        super(name, description, type, price);
         this.attackPoint = attackPoint;
         this.defensePoint = defensePoint;
         this.level = level;
         this.attribue = attribute;
         this.monsterType = monsterType;
-    }		
-    
-    public void setAttackPonit(int attackpoint) 		
-    {
+    }
+
+    public void setAttackPonit(int attackpoint) {
         this.attackPoint = attackpoint;
-    }	
-    
-    public void setDefensePoint(int defensePoint) 		
-    {
+    }
+
+    public void setDefensePoint(int defensePoint) {
         this.defensePoint = defensePoint;
-    }		
-    
-    public void setLevel(int level) 		
-    {
+    }
+
+    public void setLevel(int level) {
         this.level = level;
-    }	
-    
-    public void setAttribute(String attribute) 		
-    {
+    }
+
+    public void setAttribute(String attribute) {
         this.attribue = attribute;
-    }		
-    
-    public void setMonsterType(String monsterType) 		
-    {
+    }
+
+    public void setMonsterType(String monsterType) {
         this.monsterType = monsterType;
-    }		
-    
-    public int getAttackPonit() 		
-    {
+    }
+
+    public int getAttackPonit() {
         return attackPoint;
-    }		
-    
-    public int getDefensePonit() 		
-    {
+    }
+
+    public int getDefensePonit() {
         return defensePoint;
-    }	
-    
-    public int getLevel() 		
-    {
+    }
+
+    public int getLevel() {
         return level;
-    }	
-    
-    public String getAttribute() 		
-    {
+    }
+
+    public String getAttribute() {
         return attribue;
-    }		
-    
-    public String getMonsterType() 		
-    {
+    }
+
+    public String getMonsterType() {
         return monsterType;
-    }		
-    
-    public String getName() 		
-    {
+    }
+
+    public String getName() {
         return name;
-    }	
-    
-    public String getDescription() 		
-    {
+    }
+
+    public String getDescription() {
         return description;
-    }		
-    
-    public String getCardNumber() 		
-    {
+    }
+
+    public String getCardNumber() {
         return cardNumber;
-    }	
+    }
 }
