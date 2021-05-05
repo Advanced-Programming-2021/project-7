7bdb7561a58e56a319a46b8b2ff778cdc8e13773
mahdi-shafee <82397556+mahdi-shafee@users.noreply.github.com>
2021-05-06 03:33:29 +0430

    Update DeckMenu.java

--- Project/src/main/java/Menus/DeckMenu.java
+++ Project/src/main/java/Menus/DeckMenu.java
@@ -6,8 +6,7 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class DeckMenu
-{
+class DeckMenu {
     private Player loggedInUser;
 
 
@@ -22,6 +21,9 @@ class DeckMenu
             else if (command.matches("^deck rm-card (?:(?:--card|--deck) ([^ ]+) (--side)*?){2,3}$"))
                 removeCardFromDeck(username, command);
             else if (command.matches("^deck show --all")) showAllDecks(username);
+            else if (command.matches("^deck show (?:(?:--deck-name) ([^ ]+) (--side)*?){1,2}$"))
+                showDeck(username, command);
+            else if (command.matches("^deck show --cards$")) showCards(username);
             else if (command.matches("^menu enter (profile|duel|deck|shop|scoreboard)$"))
                 System.out.println("menu navigation is not possible");
             else if (command.matches("^menu show-current$")) System.out.println("deck");
@@ -29,36 +31,36 @@ class DeckMenu
                 return;
             } else System.out.println("invalid command!");
         }
-    }		
-    
+    }
+
     private void createDeck(String username, String command) throws IOException {
         String pattern = "^deck create ([^ ]+)$";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(command);
         m.find();
         String deckName = m.group(1);
-        if(Deck.doesDeckExist(deckName)){
+        if (Deck.doesDeckExist(deckName)) {
             System.out.printf("deck with name %s already exists\n", deckName);
             return;
         }
         System.out.println("deck created successfully!");
         new Deck(deckName, username);
         FileHandler.updatePlayers();
-    }		
-    
+    }
+
     private void deleteDeck(String username, String command) throws IOException {
         String pattern = "^deck delete ([^ ]+)$";
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(command);
         m.find();
         String deckName = m.group(1);
-        if(Deck.getDeckByNames(deckName, username) == null){
+        if (Deck.getDeckByNames(deckName, username) == null) {
             System.out.printf("deck with name %s does not exist\n", deckName);
             return;
         }
         System.out.println("deck delete successfully!");
         Player.getPlayerByUsername(username).removeDeck(deckName);
-        if(Player.getActiveDeckByUsername(username).getDeckName().equals(deckName)){
+        if (Player.getActiveDeckByUsername(username).getDeckName().equals(deckName)) {
             Player.getPlayerByUsername(username).removeActiveDeck();
         }
         FileHandler.updatePlayers();
@@ -70,7 +72,7 @@ class DeckMenu
         Matcher m = r.matcher(command);
         m.find();
         String deckName = m.group(1);
-        if(Deck.getDeckByNames(deckName, username) == null){
+        if (Deck.getDeckByNames(deckName, username) == null) {
             System.out.printf("deck with name %s does not exist\n", deckName);
             return;
         }
@@ -78,30 +80,29 @@ class DeckMenu
         Player.getPlayerByUsername(username).setActiveDeck(deckName);
         FileHandler.updatePlayers();
     }
-    
+
     private void addCardToDeck(String username, String command) throws IOException {
         String cardName = CommonTools.takeNameOutOfCommand(command, "--card");
         String deckName = CommonTools.takeNameOutOfCommand(command, "--deck");
-        if(!addCardValidity(cardName, deckName, username)) return;
+        if (!addCardValidity(cardName, deckName, username)) return;
         Deck deck = Deck.getDeckByNames(deckName, username);
         Player player = Player.getPlayerByUsername(username);
-        if(!command.contains("--side")){
-            if(deck.isMainDeckFull()){
+        if (!command.contains("--side")) {
+            if (deck.isMainDeckFull()) {
                 System.out.println("main deck is full");
                 return;
             }
-            if(deck.isThereThreeCards(Card.getCardByName(cardName))){
+            if (deck.isThereThreeCards(Card.getCardByName(cardName))) {
                 System.out.printf("there are already three cards with name %s in deck %s\n", cardName, deckName);
                 return;
             }
             deck.addCardToMainDeck(Card.getCardByName(cardName));
-        }
-        else{
-            if(deck.isSideDeckFull()){
+        } else {
+            if (deck.isSideDeckFull()) {
                 System.out.println("side deck is full");
                 return;
             }
-            if(deck.isThereThreeCards(Card.getCardByName(cardName))){
+            if (deck.isThereThreeCards(Card.getCardByName(cardName))) {
                 System.out.printf("there are already three cards with name %s in deck %s\n", cardName, deckName);
                 return;
             }
@@ -112,18 +113,17 @@ class DeckMenu
         System.out.println("card added to deck successfully");
         FileHandler.updatePlayers();
     }
-    
+
     private void removeCardFromDeck(String username, String command) throws IOException {
         String cardName = CommonTools.takeNameOutOfCommand(command, "--card");
         String deckName = CommonTools.takeNameOutOfCommand(command, "--deck");
         boolean side = command.contains("--side");
-        if(!removeCardValidity(cardName, deckName, username, side)) return;
+        if (!removeCardValidity(cardName, deckName, username, side)) return;
         Deck deck = Deck.getDeckByNames(deckName, username);
         Player player = Player.getPlayerByUsername(username);
-        if(!command.contains("--side")){
+        if (!command.contains("--side")) {
             deck.removeCardFromMainDeck(Card.getCardByName(cardName));
-        }
-        else{
+        } else {
             deck.removeCardFromSideDeck(Card.getCardByName(cardName));
         }
         player.removeCard(Card.getCardByName(cardName));
@@ -131,60 +131,57 @@ class DeckMenu
         FileHandler.updatePlayers();
     }
 
-    private boolean addCardValidity(String cardName, String deckName, String username){
-        if(cardName == null | deckName == null){
+    private boolean addCardValidity(String cardName, String deckName, String username) {
+        if (cardName == null | deckName == null) {
             System.out.println("invalid command");
             return false;
         }
         Player player = Player.getPlayerByUsername(username);
-        if(!player.doesCardExist(cardName)){
+        if (!player.doesCardExist(cardName)) {
             System.out.printf("card with name %s does not exist\n", cardName);
             return false;
         }
-        if(Deck.getDeckByNames(deckName, username) == null){
+        if (Deck.getDeckByNames(deckName, username) == null) {
             System.out.printf("deck with name %s does not exist\n", deckName);
             return false;
         }
         return true;
     }
 
-    private boolean removeCardValidity(String cardName, String deckName, String username, boolean side){
-        if(cardName == null | deckName == null){
+    private boolean removeCardValidity(String cardName, String deckName, String username, boolean side) {
+        if (cardName == null | deckName == null) {
             System.out.println("invalid command");
             return false;
         }
-        if(Deck.getDeckByNames(deckName, username) == null){
+        if (Deck.getDeckByNames(deckName, username) == null) {
             System.out.printf("deck with name %s does not exist\n", deckName);
             return false;
         }
         Deck deck = Deck.getDeckByNames(deckName, username);
-        if(!side){
-            if(!Deck.getMainDeckByDeck(deck).containsKey(Card.getCardByName(cardName))){
+        if (!side) {
+            if (!Deck.getMainDeckByDeck(deck).containsKey(Card.getCardByName(cardName))) {
                 System.out.printf("card with name %s does not exist in main deck\n", cardName);
                 return false;
             }
-        }
-        else{
-            if(!Deck.getSideDeckByDeck(deck).containsKey(Card.getCardByName(cardName))){
+        } else {
+            if (!Deck.getSideDeckByDeck(deck).containsKey(Card.getCardByName(cardName))) {
                 System.out.printf("card with name %s does not exist in side deck\n", cardName);
                 return false;
             }
         }
         return true;
     }
-    
-    private void showAllDecks(String username)
-    {
+
+    private void showAllDecks(String username) {
         Player.getPlayerByUsername(username).showDecks();
-    }		
-    
-    private void showDeck(Matcher matcher) 		
-    {
-        
-    }		
-    
-    private void showCards() 		
-    {
-        
-    }		
+    }
+
+    private void showDeck(String username, String command) {
+        String deckName = CommonTools.takeNameOutOfCommand(command, "--deck-name");
+
+    }
+
+    private void showCards(String username) {
+        Player.getPlayerByUsername(username).showCards();
+    }
 }
