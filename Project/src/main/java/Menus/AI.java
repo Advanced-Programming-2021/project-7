894c1b25078499a10c0ae6ab1ea44036cfe59fbf
mahdi-shafee <82397556+mahdi-shafee@users.noreply.github.com>
2021-06-22 20:29:00 +0430

    Update AI.java

--- Project/src/main/java/Menus/AI.java
+++ Project/src/main/java/Menus/AI.java
@@ -4,22 +4,22 @@ public class AI {
     private GameDeck AIDeck;
     private GameDeck playerDeck;
     private Phase phase;
+    private boolean isSelected = false;
+
+    public void updateAI(GameDeck AIDeck, GameDeck playerDeck, Phase phase){
+        this.AIDeck = AIDeck;
+        this.playerDeck = playerDeck;
+        this.phase = phase;
+    }
 
     public String decision(GameDeck AIDeck, GameDeck playerDeck, Phase phase){
         String decision = null;
-        if (phase == Phase.draw){
-            decision = drawPhase();
-        } else if (phase == Phase.standby){
-            decision = standbyPhase();
-        } else if (phase == Phase.main1){
-
-        } else if (phase == Phase.battle){
-
-        } else if (phase == Phase.main2){
-
-        } else if (phase == Phase.end){
-
-        }
+        if (phase == Phase.draw) decision = drawPhase();
+        else if (phase == Phase.standby) decision = standbyPhase();
+        else if (phase == Phase.main1) decision = main1Phase();
+        else if (phase == Phase.battle) decision = battlePhase();
+        else if (phase == Phase.main2) decision = main2Phase();
+        else if (phase == Phase.end) decision = endPhase();
         return decision;
     }
 
@@ -32,11 +32,31 @@ public class AI {
     }
 
     private String main1Phase(){
-        for (int i = 0; i <= AIDeck.getInHandCards().size(); i++){
-            if (AIDeck.getInHandCards().get(i).getType().equals("Spell")){
-                return "select --hand " + i + 1;
+        if (!isSelected){
+            if (!AIDeck.isSpellZoneFull()){
+                for (int i = 0; i <= AIDeck.getInHandCards().size(); i++){
+                    if (AIDeck.getInHandCards().get(i).getType().equals("Spell")){
+                        isSelected = true;
+                        return "select --hand " + i + 1;
+                    }
+                }
             }
+        } else {
+            isSelected = false;
+            return "set";
         }
+        return "next phase";
+    }
+
+    private String battlePhase(){
+        return "next phase";
+    }
+
+    private String main2Phase(){
+        return "next phase";
+    }
+
+    private String endPhase(){
         return "next phase";
     }
 }
