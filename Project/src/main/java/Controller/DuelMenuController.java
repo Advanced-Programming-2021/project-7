49b4163d515ef028594b7beeef3d62436ee037a2
Shayan <s.salehi1381@gmail.com>
2021-07-07 11:24:59 +0430

    DuelMenuUpdated

--- Project/src/main/java/Controller/DuelMenuController.java
+++ Project/src/main/java/Controller/DuelMenuController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.Player;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -7,12 +8,54 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class DuelMenuController {
+    private String firstPlayer = Player.getActivePlayer().getUsername();
+    private String secondPlayer;
+
     private Stage stage;
     private Scene scene;
     private AnchorPane root;
+
+    public void newRound(ActionEvent event) {
+
+    }
+
+    public void newMatch(ActionEvent event) {
+
+    }
+
+    public void getSecondPlayerName() {
+        secondPlayer = JOptionPane.showInputDialog("Enter your opponent's nickname:");
+        isDuelValid(firstPlayer, secondPlayer);
+        JOptionPane.showMessageDialog(null, "hey");
+    }
+
+    private boolean isDuelValid(String player1, String player2){
+        if (Player.getPlayerByUsername(player2) == null) {
+            JOptionPane.showMessageDialog(null, "there is no player with this username");
+            return false;
+        }
+        if (Player.getActiveDeckByUsername(player1) == null) {
+            JOptionPane.showMessageDialog(null, player1 + " has no active deck");
+            return false;
+        }
+        if (Player.getActiveDeckByUsername(player2) == null) {
+            JOptionPane.showMessageDialog(null, player2 + " has no active deck");
+            return false;
+        }
+        if (!Player.getActiveDeckByUsername(player1).isDeckValid()) {
+            JOptionPane.showMessageDialog(null, player1 + "'s deck is invalid");
+            return false;
+        }
+        if (!Player.getActiveDeckByUsername(player2).isDeckValid()) {
+            JOptionPane.showMessageDialog(null, player2 + "'s deck is invalid");
+            return false;
+        }
+        return true;
+    }
 
     public void back(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/main_menu_view.fxml"));
