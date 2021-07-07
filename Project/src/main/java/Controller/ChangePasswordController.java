9247f477bfc65fe9932d8860e615fbabe780bba9
Shayan <s.salehi1381@gmail.com>
2021-07-07 16:53:31 +0430

    ChangePassword Finished

--- Project/src/main/java/Controller/ChangePasswordController.java
+++ Project/src/main/java/Controller/ChangePasswordController.java
@@ -1,9 +1,14 @@
 package Controller;
 
+import Model.Player;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -14,8 +19,35 @@ public class ChangePasswordController {
     private Scene scene;
     private AnchorPane root;
 
-    public void change(ActionEvent event) {
+    @FXML
+    private TextField newPasswordTextField;
+    @FXML
+    private TextField oldPasswordTextField;
+    @FXML
+    private Label commandLabel;
 
+    public void change(ActionEvent event) throws IOException {
+        String newPassword = newPasswordTextField.getText();
+        String oldPassword = oldPasswordTextField.getText();
+        if (oldPassword.equals("")) {
+            commandLabel.setText("You have not entered your old password!");
+            return;
+        }
+        if (newPassword.equals("")) {
+            commandLabel.setText("You have not entered your new password!");
+            return;
+        }
+        Player player = Player.getActivePlayer();
+        if (!oldPassword.equals(player.getPassword())) {
+            commandLabel.setText("Old password is wrong!");
+            return;
+        }
+        player.setPassword(newPassword);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Change Password");
+        alert.setHeaderText("Password changed successfully!");
+        alert.showAndWait();
+        back(event);
     }
 
     public void back(ActionEvent event) throws IOException {
