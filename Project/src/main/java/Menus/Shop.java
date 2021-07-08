7895dcdeda6c8f52741292601ca9679ba4908a5e
Armin <armin.saghafian@gmail.com>
2021-07-08 13:37:47 +0430

    Auto stash before merge of "main" and "origin/main"

--- Project/src/main/java/Menus/Shop.java
+++ Project/src/main/java/Menus/Shop.java
@@ -96,19 +96,6 @@ public class Shop extends Application{
         Shop.username = username;
         start(MainProgramView.stage);
         System.out.println("Welcome to Shop");
-//        while (true) {
-//            String command = CommonTools.scan.nextLine();
-//            if (command.matches("^shop buy .+$")) buyCard(username, command);
-//            else if (command.matches("^shop show --all$")) showAll();
-//            else if (command.matches("^menu enter (profile|duel|deck|shop|scoreboard)$"))
-//                System.out.println("menu navigation is not possible");
-//            else if (command.matches("^menu show-current$")) System.out.println("shop");
-//            else if (command.matches("^increase --money \\d+$")) increaseMoney(command, username);
-//            else if (command.matches("^menu exit$")) {
-//                System.out.println("MainMenu");
-//                return;
-//            } else System.out.println("invalid command!");
-//        }
     }
 
     public void increaseMoney(String command, String playerName) {
---
+++ Project/src/main/java/View/GameBoardView.java
@@ -0,0 +1,51 @@
+package View;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class GameBoardView extends Application {
+    @FXML
+    public GridPane enemyGrid;
+    public GridPane myGrid;
+    public ImageView selectedCardShow;
+
+    @FXML
+    public void initialize(){
+        selectedCardShow.setImage(new Image(getClass().getResource("/Images/Cards/Unknown.jpg").toExternalForm()));
+        enemyGrid.setHgap(30);
+        enemyGrid.setVgap(25);
+        myGrid.setHgap(30);
+        myGrid.setVgap(25);
+        for (int i = 0; i < 5; i++) {
+            for (int i1 = 0; i1 < 2; i1++) {
+                Rectangle rectangle = new Rectangle(60,90);
+                rectangle.setFill(Color.BLACK);
+                Rectangle rectangle1 = new Rectangle(60,90);
+                rectangle.setFill(Color.RED);
+                enemyGrid.add(rectangle,i, i1);
+                myGrid.add(rectangle1,i, i1);
+            }
+        }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/game_board.fxml"));
+        Scene scene = new Scene(root,1020,800);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void run() {
+        launch();
+    }
+}
